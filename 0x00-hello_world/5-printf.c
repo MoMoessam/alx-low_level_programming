93eb29aa402f9139be10ba6bf98322a11bd72349
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-13 19:47:31 -0700

    .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/*
- *main is the entry point
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-/*
- * main is the entry ppoint
-*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
