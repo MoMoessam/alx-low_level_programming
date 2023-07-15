e96c962cab151a96d9c7734568e5851c3acd8cf0
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-14 17:19:31 -0700

    mbygo4 belzo2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/*
- * This is a function which determines
- * if a random variable chosen by the system is positive or negative
- * main - entry [point
- */
+/**
+  * main - Entry point
+  *
+  * Description: 'the program's description'
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	int n;
