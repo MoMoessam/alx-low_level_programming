da7468f453ea6e8eb183af06652a0de5913f2257
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-27 16:59:57 -0700

    edited file 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,16 +14,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		i = 1;
 	}
 	if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		i = 0;
 	}
 	if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		i = -1;
 	}
+	return (i);
 }
