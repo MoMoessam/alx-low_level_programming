ecfd3fe5c614583dc5352ace1a10b8275efe395f
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-27 08:01:57 -0700

    added 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*
+* print_alphabet: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n =< 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar("\n");
+
+	return (0);
+}
