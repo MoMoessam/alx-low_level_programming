f8ee6039e157fad75b891846ff50b7c4a888452e
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-27 19:32:54 -0700

    .

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,6 +13,10 @@ void jack_bauer(void)
 			for (m1 = 0; m1 <= 5; m1++)
 				for (m2 = 0; m2 <= 9; m2++)
 				{
+					if (h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
+					{
+						break;
+					}
 					_putchar(h1 + '0');
 					_putchar(h2 + '0');
 					_putchar(':');
