e12a272155a8d07c065033babab7991cb37bb960
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-28 18:43:33 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,15 +10,15 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			if (i * j >= 10)
 			{
 			_putchar(((i * j) / 10) + '0');
 			_putchar(((i * j) % 10) + '0');
-			if (j != 9)
+			if (j != n - 1)
 			{
 			_putchar(',');
 			_putchar(' ');
@@ -29,7 +29,7 @@ void print_times_table(int n)
 				if (j > 0)
 					_putchar(' ');
 				_putchar((i * j) + '0');
-				if (j != 9)
+				if (j != n - 1)
 				{
 				_putchar(',');
 				_putchar(' ');
