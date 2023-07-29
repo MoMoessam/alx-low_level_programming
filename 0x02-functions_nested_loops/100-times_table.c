4a5bdee732c4156189efa623a0b218c4eb283d03
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-29 07:54:53 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,59 +2,46 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- *
- *@n: skjhfa
+ * @n: number of the times table
  */
-
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, k;
 
-	if (n < 15 && n > 0)
-		for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n+1; j++)
+		for (i = 0; i <= n; i++)
 		{
-			if (i * j >=10)
+			for (j = 0; j <= n; j++)
 			{
-			if (i * j >= 10 && i * j < 100)
-			{
-			_putchar(((i * j) / 10) + '0');
-			_putchar(((i * j) % 10) + '0');
-			if (j != n)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			}
-			}
-			if (i * j >= 100)
-			{
-				int l = (i * j) / 10;
-
-				putchar(((i * j) / 100) + '0');
-				putchar((l % 10) + '0');
-				putchar(((i * j) % 10) + '0');
-				if (j != n)
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-				}
-			}
-			}
-			if (i * j < 10)
-			{
-				if (j > 0)
 					_putchar(' ');
-				_putchar((i * j) + '0');
-				if (j != n)
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
