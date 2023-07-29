fe99ea64bfd0f04cac53c363b47e4235e754e962
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-28 18:36:23 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,14 @@ void print_to_98(int n)
 	if (n > 98)
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 	if (n < 98)
 		for (i = n; i < 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 		}
 	if (n == 98)
 		printf("98");
