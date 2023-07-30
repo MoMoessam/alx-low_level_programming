656a87f5d4e4450f118c7ac158fc1234a9adb9f8
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-30 09:36:26 -0700

    .

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * main - function that prints first 50 fibbonaci numbers
+  *
+  * Return: always 0 (success)
+  *
+  */
+
+int main(void)
+{
+long int sum1 = 1, sum2 = 2, i;
+
+for (i = 0; i < 50; i++)
+{
+	printf("%ld, ", sum1);
+	printf("%ld", sum2);
+	if (i != 49)
+         printf(", ");
+	sum1 = sum2 + sum1;
+	sum2 = sum1 + sum2;
+}
+printf("\n");
+
+return (0);
+}
