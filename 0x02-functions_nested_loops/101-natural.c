afd7803c5b96334a7d55c3f4bcd0cddd9db14bb1
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-30 07:28:04 -0700

    .

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * main - prints the numbers that are multiples of 3 or 5
+ *
+ * Description: a nested for loop that ittirates from 0 to 1024
+ *
+ */
+
+void main
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+
+	printf("%d\n", sum);
+}
