5d409567600924f4847d5486035b7b7104682509
Mohamed Essam <kingslasher3000@gmail.com>
2023-07-31 06:57:33 -0700

    added file 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * _isupper - checks whether c is upper or lowercase
+  *
+  *@c: the number being checked
+  *
+  * Return: gives 0 if lowercase and 1 if uppercase
+  */
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			return (0);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			return (1);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this is a directory for the 0x04 c task
+below is an explanation for each file :
